34ce7fe4f1ecabc8b638bf75f5a4c56ad73e0d9e
Tarje Pladsen <tarje.pladsen@norgesgruppen.no>
2024-05-30 19:48:57 +0200

    Upgrade to Storybook 8.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,20 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "./local-preset.js",
+    "@chromatic-com/storybook",
   ],
+
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
+
   staticDirs: ["../public"],
 };
 export default config;
